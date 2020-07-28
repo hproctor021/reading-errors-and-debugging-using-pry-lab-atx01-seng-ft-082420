90bfb762838e7e0f4d8a782def1869a924ffb369
Haley Proctor <hproctor021@gmail.com>
2020-07-28 20:07:35 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -1,7 +1,7 @@
 require 'pry'
 
 def generate_star_date
-  star_date = (rand(100000) + 400000) / 10.0
+  (rand(100000) + 400000) / 10.0
 end
 
 
@@ -19,6 +19,6 @@ end
 
 def engage
   date = generate_star_date
-  state_log(star_date)
+  state_log(date)
   greet_crew(crew)
 end
